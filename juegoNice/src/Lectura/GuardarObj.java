6515900c64a72343ddb01b983c7ed6b5ce8636e4
Mario Tzul <marioajpop201931177@cunoc.edu.gt>
2021-06-30 11:46:05 -0600

    guardando dadtos

--- juegoNice/src/Lectura/GuardarObj.java
+++ juegoNice/src/Lectura/GuardarObj.java
@@ -1,14 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Lectura;
 
-/**
- *
- * @author Matzzeus
- */
+import jugador.Player;
+import javax.swing.*;
+import java.io.*;
+
 public class GuardarObj {
-    
+
+    public static String directorio = System.getProperty("user.dir");
+
+    public static void GuardarObj (Object objeto){
+
+        File archivo = new File(directorio + "/Jugadores");
+        archivo.mkdir();
+
+        try{
+            ObjectOutputStream fichero;
+
+            if(objeto instanceof Player){
+                fichero = new ObjectOutputStream(new FileOutputStream(archivo.getPath()+ "/"+ ((Player) objeto).getNOMBRE() +"." + "jugador"));
+            }
+            else{
+                fichero = null;
+            }
+
+            fichero.writeObject(objeto);
+            fichero.close();
+
+        }catch (FileNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, "Error al guardar, ponga nombre al archivo");
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(null, "Error al guardar en la salida");
+            System.out.println(ex);
+        }
+    }
 }

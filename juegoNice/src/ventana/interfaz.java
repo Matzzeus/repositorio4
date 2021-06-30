c5966827282623599b8c64d1ba44b12966920be8
Mario Tzul <marioajpop201931177@cunoc.edu.gt>
2021-06-30 12:37:07 -0600

    agregar imagen de fondos

--- juegoNice/src/ventana/interfaz.java
+++ juegoNice/src/ventana/interfaz.java
@@ -5,6 +5,7 @@
  */
 package ventana;
 
+import Visual.VisualMenu;
 import static java.time.Clock.system;
 
 /**
@@ -31,9 +32,6 @@ public class interfaz extends javax.swing.JFrame {
     private void initComponents() {
 
         jugar = new javax.swing.JButton();
-        jugadores = new javax.swing.JButton();
-        reportes = new javax.swing.JButton();
-        salir = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         fondo = new javax.swing.JLabel();
 
@@ -43,37 +41,17 @@ public class interfaz extends javax.swing.JFrame {
 
         jugar.setBackground(new java.awt.Color(204, 255, 204));
         jugar.setForeground(new java.awt.Color(0, 0, 0));
-        jugar.setText("JUGAR");
-        getContentPane().add(jugar, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 20, -1, -1));
-
-        jugadores.setBackground(new java.awt.Color(204, 255, 204));
-        jugadores.setForeground(new java.awt.Color(0, 0, 0));
-        jugadores.setText("JUGADORES");
-        getContentPane().add(jugadores, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 20, 100, -1));
-
-        reportes.setBackground(new java.awt.Color(204, 255, 204));
-        reportes.setForeground(new java.awt.Color(0, 0, 0));
-        reportes.setText("REPORTES");
-        reportes.addActionListener(new java.awt.event.ActionListener() {
+        jugar.setText("INGRESAR AL JUEGO");
+        jugar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                reportesActionPerformed(evt);
+                jugarActionPerformed(evt);
             }
         });
-        getContentPane().add(reportes, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 90, -1, -1));
-
-        salir.setBackground(new java.awt.Color(204, 255, 204));
-        salir.setForeground(new java.awt.Color(0, 0, 0));
-        salir.setText("SALIR");
-        salir.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                salirActionPerformed(evt);
-            }
-        });
-        getContentPane().add(salir, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 90, 70, -1));
+        getContentPane().add(jugar, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 80, -1, -1));
 
         jLabel1.setForeground(new java.awt.Color(0, 153, 0));
         jLabel1.setText("BIENVENIDOS");
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 20, -1, -1));
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 10, 120, 40));
 
         fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/escaleraaaaaaaaaaaaaaaaaaaas.png"))); // NOI18N
         getContentPane().add(fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(-3, 0, 400, 146));
@@ -81,13 +59,14 @@ public class interfaz extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void reportesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_reportesActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_reportesActionPerformed
-
-    private void salirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_salirActionPerformed
-       System.exit(0);
-    }//GEN-LAST:event_salirActionPerformed
+    private void jugarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jugarActionPerformed
+       VisualMenu a = new VisualMenu();
+       a.setVisible(true);
+      
+           VisualMenu visualMenu = new VisualMenu();
+        visualMenu.marco();
+       
+    }//GEN-LAST:event_jugarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -127,9 +106,6 @@ public class interfaz extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel fondo;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JButton jugadores;
     private javax.swing.JButton jugar;
-    private javax.swing.JButton reportes;
-    private javax.swing.JButton salir;
     // End of variables declaration//GEN-END:variables
 }

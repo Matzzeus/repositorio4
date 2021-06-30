5941ab8c322f835bee12d25577017de15b213af3
Mario Tzul <marioajpop201931177@cunoc.edu.gt>
2021-06-30 12:58:08 -0600

    --

--- juegoNice/src/Visual/Pintar.java
+++ juegoNice/src/Visual/Pintar.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Visual.MenuOpcion;
+package Visual;
 
-/**
- *
- * @author Matzzeus
- */
-public class Pintar {
-    
+import javax.swing.*;
+
+public interface Pintar {
+    public void pintarBoton(JButton boton);
 }
